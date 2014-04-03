f3311e8ec65193409023cf475ed0f94be92c15d4
Andres Gomez <agomez@igalia.com>
2014-04-03 23:23:35 +0300

    Added launcher, icon, boostrap script and fixed latest dependecy bits

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BASE_PATH="/opt/epiphany/"
+
+pushd ${BASE_PATH}
+git clone git://git.gnome.org/jhbuild jhbuild.git
+popd
--- re-compile-and-set-as-default.sh
+++ re-compile-and-set-as-default.sh
@@ -6,8 +6,10 @@ pushd ${BASE_PATH}/jhbuild.git && git pull
 ./autogen.sh --prefix=${BASE_PATH}/jhbuild-install && make && make install
 popd
 ${BASE_PATH}/jhbuild-webkit/install-dependencies
-${BASE_PATH}/jhbuild-install/bin/jhbuild sysdeps --install
-${BASE_PATH}/jhbuild-install/bin/jhbuild -f ${BASE_PATH}/jhbuildrc make cairo fonts fontconfig freetype6 harfbuzz libxml2 gdk-pixbuf gtk+ glib glib-networking gnome-icon-theme gnome-icon-theme-symbolic gnome-themes-standard gtk-doc libsoup atk at-spi2-core at-spi2-atk gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-libav xserver libsecret dconf vala WebKit epiphany --nodeps
-pushd ${HOME}/.local/share/applications && ln -s ${BASE_PATH}/jhbuild-epiphany-browser.desktop
-popd
-
+#${BASE_PATH}/jhbuild-install/bin/jhbuild sysdeps --install
+# ${BASE_PATH}/jhbuild-install/bin/jhbuild -f ${BASE_PATH}/jhbuildrc build -f --nodeps
+${BASE_PATH}/jhbuild-install/bin/jhbuild -f ${BASE_PATH}/jhbuildrc build --nodeps
+cp ${BASE_PATH}/jhbuild-epiphany-browser.desktop ${HOME}/.local/share/applications/
+update-desktop-database ${HOME}/.local/share/applications
+# gnome-control-center info
+xdg-mime default jhbuild-epiphany-browser.desktop x-scheme-handler/http x-scheme-handler/https text/html application/xhtml+xml text/xml
